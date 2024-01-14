ccacfd82d4647380c34a4472a22f43211ecd43bf
Phong Nguyen <nnphong1904@gmail.com>
2024-01-14 13:43:45 +0700

    Initial commit

---
+++ fish/config.fish
@@ -0,0 +1,22 @@
+#PATH
+set --export ANDROID $HOME/Library/Android;
+set --export ANDROID_HOME $ANDROID/sdk;
+set -gx PATH $ANDROID_HOME/tools $PATH;
+set -gx PATH $ANDROID_HOME/tools/bin $PATH;
+set -gx PATH $ANDROID_HOME/platform-tools $PATH;
+set -gx PATH $ANDROID_HOME/emulator $PATH
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+set --export JAVA_HOME /Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home;
+set -gx PATH $JAVA_HOME/bin $PATH;
+
+set -gx LDFLAGS "-L/opt/homebrew/opt/jpeg/lib"
+set -gx CPPFLAGS "-I/opt/homebrew/opt/jpeg/include"
+
+set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/jpeg/lib/pkgconfig"
+
+# ~/.config/fish/config.fish
+# You must call it on initialization or listening to directory switching won't work
+load_nvm > /dev/stderr
+
+starship init fish | source
